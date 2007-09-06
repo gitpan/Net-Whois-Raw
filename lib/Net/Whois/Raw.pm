c67fe14052458d0aabac72e2f473ee3f49486cca
Walery Studennikov <despair@cpan.org>
2007-09-06 08:56:05 +0000

    Import of DESPAIR/Net-Whois-Raw-1.31 from CPAN.
    gitpan-cpan-distribution: Net-Whois-Raw
    gitpan-cpan-version:      1.31
    gitpan-cpan-path:         DESPAIR/Net-Whois-Raw-1.31.tar.gz
    gitpan-cpan-author:       DESPAIR
    gitpan-cpan-maturity:     released

--- lib/Net/Whois/Raw.pm
+++ lib/Net/Whois/Raw.pm
@@ -9,7 +9,7 @@ use IO::Socket;
 
 our @EXPORT    = qw( whois get_whois );
 
-our $VERSION = '1.30';
+our $VERSION = '1.31';
 
 our ($OMIT_MSG, $CHECK_FAIL, $CHECK_EXCEED, $CACHE_DIR, $USE_CNAMES, $TIMEOUT, $DEBUG) = (0) x 7;
 our $CACHE_TIME = 60;
